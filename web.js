193b62b0e248f05545e7426537e869b6237b518b
Jason LaPorte <ironwallaby@gmail.com>
2012-03-16 07:57:42 -0400

    Less fat, more filling.

--- web.js
+++ web.js
@@ -17,125 +17,68 @@ function clamp (x) {
   return x * x
 }
 
-function locations (query, callback) {
-  var locations = []
-
-  if (query.lat && query.lon)
-    locations.push ({
-      name: "Here",
-      latitude: parseFloat (query.lat),
-      longitude: parseFloat (query.lon)
-    })
-
+app.get ("/interesting", function (req, res, next) {
   return request (
     util.format (
       "https://api.darkskyapp.com/v1/interesting/%s",
       key
     ),
-    function (err, res, storms) {
+    function (err, httpres, storms) {
       if (err)
-        return callback (err)
+        return next (err)
 
-      if (res.statusCode !== 200)
-        return callback (new Error ("Received an error from Dark Sky."))
+      if (httpres.statusCode !== 200)
+        return next (new Error ("Received an error from Dark Sky."))
 
-      var cities = {},
-          i
-
+      var i
       storms = JSON.parse (storms).storms
 
-      for (i = 0; i !== storms.length; ++i) {
-        if (cities[storms[i].city]) continue
-        cities[storms[i].city] = true
-
-        locations.push ({
+      for (i = 0; i !== storms.length; ++i)
+        storms[i] = {
           name: storms[i].city,
           latitude: storms[i].latitude,
           longitude: storms[i].longitude
-        })
-      }
+        }
 
-      return callback (null, locations)
+      return res.send (storms)
     }
   )
-}
+})
 
-function forecast (lat, lon, callback) {
+app.get ("/darksky", function (req, res, next) {
   return request (
     util.format (
       "https://api.darkskyapp.com/v1/forecast/%s/%d,%d",
       key,
-      lat,
-      lon
+      parseFloat (req.query.lat),
+      parseFloat (req.query.lon)
     ),
-    function (err, res, forecast) {
+    function (err, httpres, forecast) {
       if (err)
-        return callback (err)
+        return next (err)
 
-      if (res.statusCode !== 200)
-        return callback (new Error ("Received an error from Dark Sky."))
+      if (httpres.statusCode !== 200)
+        return next (new Error ("Received an error from Dark Sky."))
 
       forecast = JSON.parse (forecast)
 
-      if (forecast.error)
-        return callback (new Error (forecast.error))
+      var data = forecast.hourPrecipitation,
+          i = data.length,
+          k, e
 
-      return callback (null, forecast)
-    }
-  )
-}
-
-app.get ("/darksky", function (req, res, next) {
-  locations (req.query, function (err, locations) {
-    if (err)
-      return next (err)
-
-    var i = locations.length,
-        todo = i
-
-    function join (err) {
-      if (!todo)
-        return
-
-      if (err) {
-        todo = 0
-        return next (err)
+      while (i--) {
+        k = (data[i].probability * data[i].intensity - 3) / (60 - 3)
+        e = data[i].error / 150
+        data[i] = {min: clamp (k - e), max: clamp (k + e)}
       }
 
-      if (--todo)
-        return
-
-      return res.send (locations)
-    }
-
-    function addForecast (i) {
-      var loc = locations[i]
-
-      return forecast (loc.latitude, loc.longitude, function (err, forecast) {
-        if (err)
-          return join (err)
-
-        var data = forecast.hourPrecipitation,
-            i = data.length,
-            k, e
-
-        while (i--) {
-          k = (data[i].probability * data[i].intensity - 3) / (60 - 3)
-          e = data[i].error / 150
-          data[i] = {min: clamp (k - e), max: clamp (k + e)}
-        }
-
-        loc.current = forecast.currentSummary
-        loc.forecast = forecast.hourSummary
-        loc.data = data
-
-        return join (null)
+      return res.send ({
+        current: forecast.currentSummary,
+        forecast: forecast.hourSummary,
+        data: data
       })
     }
-
-    while (i--)
-      addForecast (i)
-  })
+  )
 })
 
 app.listen (8080)
